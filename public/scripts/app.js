e2bcbee354bad78534e690419dcf74891f09b0bb
Greg Davis <gdavis111@live.ca>
2018-03-01 23:26:25 +0000

    Switched to Mongo Database so tweets will persist even after shutdown, cleaned up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,13 +4,39 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function escape(str) {
+function timeSince(now, date) { // Used to log the time since posted in the bottom left corner of each tweet
+  var seconds = Math.floor(now - date) / 1000;
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {  // if statement to determine whether to specify time in seconds/minutes/hours etc
+    return interval + " years ago"; // will return months until timeSince reaches 2 years
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
+
+function escape(str) { // users cannot manipulate html when this function is called on input area
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
 }
 
-function createTweetElement(tweet) {
+function createTweetElement(tweet) {  // Creates entire html structure to be styled by pre-existing css
   let $tweet = `<article class="tweet">
                     <header>
                     <img class="avatar-pic" src="${escape(tweet.user.avatars.small)}">
@@ -22,7 +48,7 @@ function createTweetElement(tweet) {
                   </div>
                   <footer class="footer">
                     <div class="date">
-                      <p class="timeStamp">${escape(tweet.created_at)}</p>
+                      <p class="timeStamp">${escape(timeSince(Date.now(), tweet.created_at))}</p>
                     </div>
                     <div>
                       <img class="bottomLinks" src="/images/tweetLike.png">
@@ -36,18 +62,18 @@ function createTweetElement(tweet) {
 }
 
 function renderTweets(tweets) { // uses createTweetElement to turn each object into a tweet
-  tweets.forEach(function(tweet) { //
+  tweets.forEach(function(tweet) {
     const createdTweet = createTweetElement(tweet);
     $('.printed-tweet').prepend(createdTweet);
     console.log(createdTweet);
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function() { // runs when document is ready (kind of self explained)
 
-  $( ".compose" ).click(function() {
+  $( ".compose" ).click(function() { // toggles tweet input form
       $('.new-tweet').slideToggle()
-      $('.new-tweet textarea').select();
+      $('.new-tweet textarea').select(); // when toggled the text area is auto-selected for the user
   });
 
   $('#new-tweet').on('submit', function(ev) {
@@ -58,9 +84,10 @@ $(document).ready(function() {
     } else if (tweetLength <= 0) {  // checks to make sure tweet is not empty
       alert('You must enter something to tweet about!')
     } else {  // allows
+      $('.counter').text(140);
       let formData = $('#new-tweet').serialize();
       $.ajax({
-        url: "/tweets", // Figure out what this means / if its right
+        url: "/tweets",
         method: "POST",
         data: formData,
         success: function(result) {
@@ -71,7 +98,7 @@ $(document).ready(function() {
     }
   });
 
-  function loadTweets() {
+  function loadTweets() { // loads tweets on document ready
     $.ajax({
       url: '/tweets',
       method: 'GET',
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function() { // function to count down (or up) on the character limit based on user input
   $('.new-tweet form textarea').on('keyup', function() {
-    var count = $('.counter'); // bad practice?
+    var count = $('.counter');
     var characters = $(this).val().length;
     var maxChars = 140;
     count.text(maxChars - characters);
     if (characters > maxChars) {
-      count.addClass('overMax');
+      count.addClass('overMax');  // if over the character limit, text turns red
     } else {
       count.removeClass('overMax');
     }
